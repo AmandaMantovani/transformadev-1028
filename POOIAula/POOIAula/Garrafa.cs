f3a916998ca6bedb23cc737554dfd1645845542f
amanda.mantovani <amanda.mantovani@luizalabs.com>
2023-09-20 21:05:39 -0300

    Aula 1

---
+++ POOIAula/POOIAula/Garrafa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOIAula
+{
+    //classe
+    internal class Garrafa
+    {
+        //caracteristica -> atributo ou field ou campo
+        public string cor;
+        public int capacidade;
+        public string material;
+        public bool termica;
+        
+        //comportamentos
+    }
+}
---
+++ POOIAula/POOIAula/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace POOIAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nome = 0;
+
+            Garrafa garrafa = new Garrafa();
+            garrafa.cor = "preto";
+            garrafa.material = "plastico";
+            garrafa.termica = true;
+            garrafa.capacidade = 750;
+
+            Garrafa garrafa2 = new();
+            garrafa2.cor = "rosa";
+
+            Garrafa garrafa3 = new()
+            {
+                cor = "preto",
+                material = "plastico",
+                capacidade = 0,
+                termica = true
+            };
+
+        }
+    }
+}
